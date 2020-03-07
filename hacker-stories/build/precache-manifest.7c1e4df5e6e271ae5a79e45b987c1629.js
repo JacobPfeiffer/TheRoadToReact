58a5594f01ad6a73c4c721a2d27db2ad3ee09fb5
Jacob Pfeiffer <jacobpfeiffer@Jacobs-MacBook-Pro-5.local>
2020-03-06 20:35:30 -0600

    Started exercise by redifining App.js

---
+++ hacker-stories/build/precache-manifest.7c1e4df5e6e271ae5a79e45b987c1629.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526680d11dfcf800f0a8c3ed3eecee8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06ea778eea62a4bea94e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6f9d184a691df308a08",
+    "url": "/static/js/2.4ab1aea2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4ab1aea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ea778eea62a4bea94e",
+    "url": "/static/js/main.a0a52010.chunk.js"
+  },
+  {
+    "revision": "c9d5deba39440e6bb493",
+    "url": "/static/js/runtime-main.ebfaa836.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
